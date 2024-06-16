7621d331d5554ac55ad760472dfa193b62495919
Maxim Vypov <m.vypov@lipt-soft.ru>
2024-06-16 22:37:43 +0300

    migratory birds: done

--- src/main/java/com/mavy0313/hackerrank/migratory/birds/MigratoryBirds.java
+++ src/main/java/com/mavy0313/hackerrank/migratory/birds/MigratoryBirds.java
@@ -8,6 +8,37 @@ public class MigratoryBirds {
     }
 
     public int find() {
-        return 1;
+        int bird1count = 0;
+        int bird2count = 0;
+        int bird3count = 0;
+        int bird4count = 0;
+        int bird5count = 0;
+
+        for (int i = 0; i < birds.length; i++) {
+            if (birds[i] == 1) {
+                bird1count++;
+            } else if (birds[i] == 2) {
+                bird2count++;
+            } else if (birds[i] == 3) {
+                bird3count++;
+            } else if (birds[i] == 4) {
+                bird4count++;
+            } else if (birds[i] == 5) {
+                bird5count++;
+            }
+        }
+
+        int[] maximums = { bird1count, bird2count, bird3count, bird4count, bird5count };
+
+        int max = bird1count;
+        int maxType = 1;
+        for (int i = 0; i < 5; i++) {
+            if (maximums[i] > max) {
+                max = maximums[i];
+                maxType = i + 1;
+            }
+        }
+
+        return maxType;
     }
 }
--- src/test/java/com/mavy0313/hackerrank/migratory/birds/MigratoryBirdsTest.java
+++ src/test/java/com/mavy0313/hackerrank/migratory/birds/MigratoryBirdsTest.java
@@ -6,12 +6,62 @@ import static org.assertj.core.api.BDDAssertions.then;
 
 public class MigratoryBirdsTest {
     @Test
-    public void findLowestBirdIdOfMostFrequentlySighted() {
+    public void findFromOneBird() {
         int[] birds = new int[]{1};
         MigratoryBirds migratoryBirds = new MigratoryBirds(birds);
 
         int result = migratoryBirds.find();
 
         then(result).isEqualTo(1);
+    }
+
+    @Test
+    public void findFromFiveBirds() {
+        int[] birds = new int[] {1, 2, 3, 4, 5};
+        MigratoryBirds migratoryBirds = new MigratoryBirds(birds);
+
+        int result = migratoryBirds.find();
+
+        then(result).isEqualTo(1);
+    }
+
+    @Test
+    public void findFromSixBirds() {
+        int[] birds = new int[] {1, 2, 3, 4, 5, 5};
+        MigratoryBirds migratoryBirds = new MigratoryBirds(birds);
+
+        int result = migratoryBirds.find();
+
+        then(result).isEqualTo(5);
+    }
+
+    @Test
+    public void findFromMaxDuplicates() {
+        int[] birds = new int[] {1, 1, 2, 2, 3, 4, 5};
+        MigratoryBirds migratoryBirds = new MigratoryBirds(birds);
+
+        int result = migratoryBirds.find();
+
+        then(result).isEqualTo(1);
+    }
+
+    @Test
+    public void findFrom3MaxDuplicates() {
+        int[] birds = new int[] {1, 1, 2, 2, 3, 4, 5, 5};
+        MigratoryBirds migratoryBirds = new MigratoryBirds(birds);
+
+        int result = migratoryBirds.find();
+
+        then(result).isEqualTo(1);
+    }
+
+    @Test
+    public void findFromMaxOf3() {
+        int[] birds = new int[] {1, 1, 2, 2, 3, 4, 5, 5, 5};
+        MigratoryBirds migratoryBirds = new MigratoryBirds(birds);
+
+        int result = migratoryBirds.find();
+
+        then(result).isEqualTo(5);
     }
 }
