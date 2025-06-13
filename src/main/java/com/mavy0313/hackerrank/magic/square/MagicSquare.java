4b52a047d66092a987b921d593ad11bc4b849bd1
Maxim Vypov <m.vypov@lipt-soft.ru>
2025-06-13 23:18:12 +0300

    magic square - build square array starting from any index, check every square array index

--- src/main/java/com/mavy0313/hackerrank/magic/square/MagicSquare.java
+++ src/main/java/com/mavy0313/hackerrank/magic/square/MagicSquare.java
@@ -1,5 +1,6 @@
 package com.mavy0313.hackerrank.magic.square;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MagicSquare {
@@ -9,7 +10,8 @@ public class MagicSquare {
      * @return
      */
     public int convert(List<List<Integer>> square) {
-        int[] magicArray = { 4, 9, 2, 7, 6, 1, 8, 3 };
+        int[] magicArray = { 4, 9, 2, 7, 6, 1, 8, 3 }; //sum is 40
+        //                   3, 4, 9, 2, 7, 6, 1, 8
         //check middle 5
         int[] squareArray = {
                 square.get(0).get(0),
@@ -21,8 +23,29 @@ public class MagicSquare {
                 square.get(2).get(0),
                 square.get(1).get(0),
         };
-        return findCost(magicArray, squareArray);
 
+        int cost;
+        List<Integer> costs = new ArrayList<>();
+        for (int i = 0; i < squareArray.length; i++) {
+            cost = findCost(magicArray, buildSquareArrayStartingFrom(i, squareArray));
+            costs.add(cost);
+        }
+
+        return costs.stream().min(Integer::compare).orElse(-1);
+
+    }
+
+    private int[] buildSquareArrayStartingFrom(int index, int[] array) {
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (i + index < array.length) {
+                result[i] = array[i + index];
+            } else {
+                int newIdx = (i + index) - array.length;
+                result[i] = array[newIdx];
+            }
+        }
+        return result;
     }
 
     private int findCost(int[] magicArray, int[] squareArray) {
--- src/test/java/com/mavy0313/hackerrank/magic/square/MagicSquareTest.java
+++ src/test/java/com/mavy0313/hackerrank/magic/square/MagicSquareTest.java
@@ -42,4 +42,76 @@ class MagicSquareTest {
         //then
         then(result).isEqualTo(2);
     }
+
+    @Test
+    void square_with_missing_one_number_and_shifted_one_step_forward() {
+        //given
+        MagicSquare magicSquare = new MagicSquare();
+        List<List<Integer>> square =
+                List.of(
+                        List.of(4, 4, 9),
+                        List.of(8, 5, 2),
+                        List.of(1, 6, 7)
+                );
+
+        //when
+        int result = magicSquare.convert(square);
+
+        //then
+        then(result).isEqualTo(1);
+    }
+
+    @Test
+    void square_with_missing_two_numbers_and_shifted_one_step_forward() {
+        //given
+        MagicSquare magicSquare = new MagicSquare();
+        List<List<Integer>> square =
+                List.of(
+                        List.of(4, 5, 9),
+                        List.of(8, 5, 2),
+                        List.of(1, 6, 7)
+                );
+
+        //when
+        int result = magicSquare.convert(square);
+
+        //then
+        then(result).isEqualTo(2);
+    }
+
+    @Test
+    void square_with_missing_three_numbers_and_shifted_one_step_forward() {
+        //given
+        MagicSquare magicSquare = new MagicSquare();
+        List<List<Integer>> square =
+                List.of(
+                        List.of(4, 5, 9),
+                        List.of(8, 5, 1),
+                        List.of(1, 6, 7)
+                );
+
+        //when
+        int result = magicSquare.convert(square);
+
+        //then
+        then(result).isEqualTo(3);
+    }
+
+    @Test
+    void square_with_missing_one_number_and_shifted_one_step_backward() {
+        //given
+        MagicSquare magicSquare = new MagicSquare();
+        List<List<Integer>> square =
+                List.of(
+                        List.of(9, 2, 7),
+                        List.of(4, 5, 6),
+                        List.of(4, 8, 1)
+                );
+
+        //when
+        int result = magicSquare.convert(square);
+
+        //then
+        then(result).isEqualTo(1);
+    }
 }
